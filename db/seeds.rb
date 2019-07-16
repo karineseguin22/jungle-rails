05e5fe4919b013727ab4420ad792aa3676d5cf54
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-16 15:16:45 +0000

    Modify seed to add reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,14 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+##Reviews 
+
+#create a review associated with that product
+product.reviews.create!({
+  product_id: 1,
+  user_id: 1,
+  description:'Best movie ever!!!',
+  rating: 5
+})
 
 puts "DONE!"
