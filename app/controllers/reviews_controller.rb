95a65ddbe2cad71489916081c6905c62ebb137c9
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-16 22:06:36 +0000

    Add relation between user id and review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,20 @@
 class ReviewsController < ApplicationController
     def create
-        raise "Yay, I'm here!"
+        @product = Product.find(params[:product_id])
+
+        @review = @product.reviews.new(review_params)
+        @review.user_id = current_user.id
+        if @review.save
+            #redirecting to the same product show page
+            redirect_to product_path(@product)
+        else
+            #provide the path to the show html.erb from the movies folder
+            #otherwise looking by default in reviews folder
+            render 'products/show'
       end
+    end 
+    private 
+    def review_params
+        params.require(:review).permit(:rating, :description)
+    end 
 end
